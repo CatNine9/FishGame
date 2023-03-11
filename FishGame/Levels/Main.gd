90ab7c92cdab88bcbc4de62787be297cb6c95855
CatNine9 <127611022+CatNine9@users.noreply.github.com>
2023-03-11 23:59:11 +0000

    A random side is chosen for enemy to spawn, in level script.
    Output is in print only, no actual spawning yet.

--- FishGame/Levels/Main.gd
+++ FishGame/Levels/Main.gd
@@ -4,7 +4,17 @@ extends Node2D
 
 
 func _ready():
-	pass 
+	spawn_enemy()
 	
 func _process(delta):
 	pass
+
+func spawn_enemy():
+	var random_side_value = randi() % 2
+	if random_side_value == 0:
+		print("Left")
+	elif random_side_value == 1:
+		print("Right")
+	else:
+		print("Invalid side value, side value is:")
+		print(random_side_value)
