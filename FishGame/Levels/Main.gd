0ad8395aef2272f900bf929903da9acd67f9536a
CatNine9 <127611022+CatNine9@users.noreply.github.com>
2023-03-12 12:15:32 +0000

    Enemy now spawns at a random height. within window constraints.

--- FishGame/Levels/Main.gd
+++ FishGame/Levels/Main.gd
@@ -12,17 +12,20 @@ func _process(delta):
 	pass
 
 func spawn_enemy():
-	var random_side_value = randi()  % 2
+	var random_side_value = randi() % 2
 	if random_side_value == 0:
 		print("Enemy spawning Left")
 	elif random_side_value == 1:
 		print("Enemy spawning Right")
 	else:
 		print("Invalid side value, side value is: ", random_side_value)
+		
+	var random_height_value = randi_range(0, 800)
 	
 	var enemy_preload = preload("res://Fish/Enemy.tscn")
 	var enemy_spawn = enemy_preload.instantiate()
 	enemy_spawn.spawn_side = random_side_value
+	enemy_spawn.position.y = random_height_value
 	add_child(enemy_spawn)
 	
 
