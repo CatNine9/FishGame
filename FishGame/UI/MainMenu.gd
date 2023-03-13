ca093298ca7946260198e23f51438f4dcbd34b1d
CatNine9 <127611022+CatNine9@users.noreply.github.com>
2023-03-13 17:37:21 +0000

    The quit button on the Main Menu now closes the application.

--- FishGame/UI/MainMenu.gd
+++ FishGame/UI/MainMenu.gd
@@ -6,10 +6,18 @@ extends Control
 
 
 
-func _physics_process(delta):
-	pass
-
-
 func _on_start_button_button_up():
 	var game_start = load("res://Levels/Main.tscn")
 	get_tree().change_scene_to_packed(game_start)
+
+
+func _on_settings_button_button_up():
+	pass # Replace with function body.
+
+
+func _on_leader_board_button_button_up():
+	pass # Replace with function body.
+
+
+func _on_quit_button_button_up():
+	get_tree().quit()
