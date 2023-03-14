ea2c8c5146118cfa8c0ecd64e3b610c9acf7c8f9
CatNine9 <127611022+CatNine9@users.noreply.github.com>
2023-03-14 10:44:56 +0000

    Progress on death window "pages".
    Can progress from page 1 to page 2. Pages are turned on and off using the node's visibility setting.

---
+++ FishGame/Levels/DeathControl.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+@onready var page_1 = $CenterContainer/ColorRect
+@onready var page_2 = $CenterContainer/ColorRect2
+
+
+
+func _on_next_button_button_up():
+	page_1.visible = false
+	page_2.visible = true
