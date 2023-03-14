0dd7a9bf17ae16b303b459c3f7cbf242c663f734
CatNine9 <127611022+CatNine9@users.noreply.github.com>
2023-03-14 09:14:12 +0000

    The settings toggle now correctly remembers setting between the main menu and in-game and back.

--- FishGame/GlobalVariables.gd
+++ FishGame/GlobalVariables.gd
@@ -3,6 +3,9 @@ extends Node
 # Settings:
 var size_visibility = false
 
+# UI:
+var size_vis_toggle_setting = false
+
 # Game:
 var is_paused = false
 
--- FishGame/UI/Settings.gd
+++ FishGame/UI/Settings.gd
@@ -1,12 +1,24 @@
 extends Control
 
+@onready var size_vis_toggle = $CenterContainer/MenuVbox/HBoxContainer/Toggle
+
+func _ready():
+	if GlobalVariables.size_vis_toggle_setting == false:
+		size_vis_toggle.button_pressed = false
+	else:
+		size_vis_toggle.button_pressed = true
 
 
-func _on_toggle_toggled(button_pressed):
+
+func _on_toggle_button_down():
 	if GlobalVariables.size_visibility == false:
 		GlobalVariables.size_visibility = true
+		GlobalVariables.size_vis_toggle_setting = true
+		print("Size visibility is ", GlobalVariables.size_visibility)
 	else:
 		GlobalVariables.size_visibility = false
+		GlobalVariables.size_vis_toggle_setting = false
+		print("Size visibility is ", GlobalVariables.size_visibility)
 
 
 
@@ -18,3 +30,6 @@ func _on_back_button_button_up():
 		var back_to_game = load("res://Levels/Main.tscn")
 		get_tree().change_scene_to_packed(back_to_game)
 		print("Input detected!")
+
+
+
