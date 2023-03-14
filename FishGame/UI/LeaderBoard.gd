2338abca0863cad60a00d0a1a5be4b3a7ed4c559
CatNine9 <127611022+CatNine9@users.noreply.github.com>
2023-03-14 16:26:11 +0000

    A leaderboard entry now gets added when a name is submitted, but is incomplete.
    The entry gets added under the back button, so the back button needs to be in a separate box. The date doesn't yet match the player's computer date either.

--- FishGame/UI/LeaderBoard.gd
+++ FishGame/UI/LeaderBoard.gd
@@ -8,7 +8,12 @@ func _ready():
 	if GlobalVariables.player_name == null:
 		pass
 	else:
-		pass
+		var new_entry_packed = load("res://UI/example_hbox.tscn")
+		var new_entry = new_entry_packed.instantiate()
+		get_child(1, true).get_child(0, true).add_child(new_entry)
+		new_entry.name_label.text = GlobalVariables.player_name
+		#new_entry.date_label.text = 
+		new_entry.score_label.text = str(GlobalVariables.player_score)
 
 func _on_back_button_button_up():
 	GlobalVariables.player_alive = true
---
+++ FishGame/UI/example_hbox.gd
@@ -0,0 +1,7 @@
+extends HBoxContainer
+
+
+
+@onready var name_label = $ExampleNameLabel
+@onready var date_label = $ExampleDateLabel
+@onready var score_label = $ExampleScoreLabel
