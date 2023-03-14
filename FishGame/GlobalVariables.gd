cf1c8015fb212a44ced4d7c283cde982d14e1e4c
CatNine9 <127611022+CatNine9@users.noreply.github.com>
2023-03-14 17:05:48 +0000

    When making an entry, the correct system date is used.
    Other issues still not fixed.

--- FishGame/GlobalVariables.gd
+++ FishGame/GlobalVariables.gd
@@ -14,3 +14,15 @@ var player_position = Vector2(400, 300)
 var player_score = 0
 var player_alive = true
 var player_name = null
+
+# Leaderboard:
+var entry_1 = ["Cat9", "13/03/2023", str(11)]
+var entry_2 = []
+var entry_3 = []
+var entry_4 = []
+var entry_5 = []
+var entry_6 = []
+var entry_7 = []
+var entry_8 = []
+var entry_9 = []
+var entry_10 = []
--- FishGame/UI/LeaderBoard.gd
+++ FishGame/UI/LeaderBoard.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node
 
 
 
@@ -12,7 +12,10 @@ func _ready():
 		var new_entry = new_entry_packed.instantiate()
 		get_child(1, true).get_child(0, true).add_child(new_entry)
 		new_entry.name_label.text = GlobalVariables.player_name
-		#new_entry.date_label.text = 
+		var datetime_dict = Time.get_date_dict_from_system()
+		var time_format = "%d/%d/%d"
+		var time_string = time_format % [datetime_dict["day"], datetime_dict["month"], datetime_dict["year"]]
+		new_entry.date_label.text = time_string
 		new_entry.score_label.text = str(GlobalVariables.player_score)
 
 func _on_back_button_button_up():
